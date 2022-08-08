6f6b3ca866bc7e8d4d2d67c720cecc714aa11fd8
IIaJIa4eX <iiajia4ex@gmail.com>
2022-08-08 21:26:04 +0300

    Added lesson2

---
+++ Practice_part2/TimeShittyCompany/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace TimeShittyCompany.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("ok");
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok("def id");
+        }
+
+        [HttpPost("register")]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Ok("reg ок");
+        }
+
+
+        [HttpPut("update/{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok("updated");
+        }
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok("deleted");
+        }
+    }
+}
---
+++ Practice_part2/TimeShittyCompany/Controllers/InvoicesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeShittyCompany.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoicesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("ok");
+        }
+
+        [HttpPut("update/{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok("updated");
+        }
+    }
+}
