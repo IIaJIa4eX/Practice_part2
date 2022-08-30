95f741f7a6897f1c293aeac6693c072c9fcee668
IIaJIa4eX <iiajia4ex@gmail.com>
2022-08-30 22:22:56 +0300

    Added one ,ore test

--- Practice_part2/TimeShittyTester/UnitTest1.cs
+++ Practice_part2/TimeShittyTester/UnitTest1.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
+using System.Collections.Generic;
 using TimeShittyCompany.Controllers;
 using TimeShittyCompany.Models.Authorization;
+using TimeShittyCompany.Models.Common;
 using TimeShittyCompany.Services.Interfaces;
 using Xunit;
 
@@ -18,26 +20,34 @@ namespace TimeShittyTester
         private Mock<IUsersService> _userServiceMock;
         private Mock<IAuthService> _authServiceMock;
         private AuthController __controllerAuth;
+        private Mock<HttpContext> _httpContext;
 
         public UnitTest1()
         {
 
             _userServiceMock = new Mock<IUsersService>();
             _authServiceMock = new Mock<IAuthService>();
-            _controller = new CustomersController(_userServiceMock.Object, _authServiceMock.Object);
+            _httpContext = new Mock<HttpContext>();
+            _controller = new CustomersController(_userServiceMock.Object, _authServiceMock.Object)
+            {
+                ControllerContext = { HttpContext = _httpContext.Object }
+            };
             __controllerAuth = new AuthController(_authServiceMock.Object);
         }
 
         
-        //ѕришлось содавать метод Authenticate_ForTesting дл€ теста (без записи токенов в Cookie)
+        //ѕришлось создавать метод Authenticate_ForTesting дл€ теста (без записи токенов в Cookie)
         //Ќе разобралс€ как создать Response.Cookie в AuthConroller'e, через тесты
         [Fact]
         public void Auth_Tester_Ok_Result()
         {
             string testName = "testName";
             string testPass = "testPassword";
+
             _authServiceMock.Setup(mock => mock.Authenticate_TokenResp(testName, testPass)).
-                Returns(new JwtTokenResponse() { Token = "Barer asdasdsad", RefreshToken = "asdadsadasdasdasd" });
+                Returns(new JwtTokenResponse() { Token = "Barer asdasdsad",
+                    RefreshToken = "asdadsadasdasdasd"
+                });
 
             var result = __controllerAuth.Authenticate_ForTesting(testName, testPass);
             Assert.IsAssignableFrom<OkObjectResult>(result);
@@ -53,6 +63,36 @@ namespace TimeShittyTester
 
             var result = __controllerAuth.Authenticate_ForTesting(testName, testPass);
             Assert.IsAssignableFrom<BadRequestObjectResult>(result);
+        }
+
+
+        [Fact]
+        public void Add_New_Customer()
+        {
+
+            _userServiceMock.Setup(mock => mock.GetByName("Alex")).Returns(new List<User>()
+            {
+                new User()
+                {
+                Age = 27,
+                isDeleted = false,
+                Company = "FakeCompany",
+                FirstName = "Alex",
+                Email = "alex@fakemai.com",
+                LastName = "Empty",
+                Expires = DateTime.Today,
+                Id = 13,
+                Password = "admin",
+                Token = "adsasdasasdasdsad"
+                }
+            });
+
+            var result = _controller.GetByName("Alex");
+
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<IActionResult>(result);
+
+
         }
     }
 }
