a16d1dc3fb96edf6cdf86d59fbbd2ff6f3e14e5c
IIaJIa4eX <iiajia4ex@gmail.com>
2022-09-08 22:15:17 +0300

    first try

---
+++ Practice_part2/lesson3__threadpool/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+
+namespace lesson3__threadpool
+{
+        
+    public class PoolSettings
+    {
+        public int _maxThreads { get; private set; }
+
+        public int _currentThreadsCount { get; set; }
+        public PoolSettings(int maxThreads)
+        {
+            _currentThreadsCount = 0;
+            _maxThreads = maxThreads;
+        }
+
+        public bool ThreadsNumberControl()
+        {
+            return _currentThreadsCount <= _maxThreads;
+        }
+       
+    }
+
+    public  class TestPool
+    {
+
+        PoolSettings _pullControll;
+
+        public TestPool(int maxThreads)
+        {
+            _pullControll = new PoolSettings(maxThreads);
+
+        }
+
+        public void DoStuff(WaitCallback callBack)
+        {
+            lock (_pullControll)
+            {
+                if(_pullControll.ThreadsNumberControl())
+                {
+                    ThreadPool.QueueUserWorkItem(callBack);
+                }
+                else
+                {
+                    throw new Exception("To much threads ot use");
+                }
+                
+                
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestPool test = new TestPool(5);
+
+            test.DoStuff(new WaitCallback(SomeHighLoadCalculation));
+
+        }
+
+        public static void SomeHighLoadCalculation(object state)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(1 + i);
+            }
+        }
+            
+    }
+}
