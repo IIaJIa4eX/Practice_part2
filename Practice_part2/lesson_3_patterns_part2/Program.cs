132448a2793794366f9652112f20a36529cea7ab
IIaJIa4eX <iiajia4ex@gmail.com>
2022-09-15 21:15:10 +0300

    added lesso 4

---
+++ Practice_part2/lesson_3_patterns_part2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson_3_patterns_part2
+{
+    class Program
+    {
+        public interface IGood
+        {
+            Good GetGood(string url);
+            IReadOnlyList<Good> GetGoods(string url);
+
+        }
+
+        public class Good 
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
