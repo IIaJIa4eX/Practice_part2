1a85d6475eb4f331ce638b97cf8630a540ad53a8
IIaJIa4eX <iiajia4ex@gmail.com>
2022-09-19 21:59:15 +0300

    added project for a lesson 5

---
+++ Practice_part2/lesson_3_patterns_part3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson_3_patterns_part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
