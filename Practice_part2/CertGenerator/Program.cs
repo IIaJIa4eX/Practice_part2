a5c151b4daa55177a1f39452d5aedaa82318ed4b
Alexander <iiajia4ex@gmail.com>
2022-10-26 22:34:03 +0300

    added child ccert creation

--- Practice_part2/CertGenerator/Program.cs
+++ Practice_part2/CertGenerator/Program.cs
@@ -13,9 +13,37 @@
                 OutFolder = @"C:\testcert"
                 
             };
-            CertificateGenerateProvider certGenerator = new CertificateGenerateProvider();
-            certGenerator.GenerateRootCertificate(certificationConfiguration);
+            //CertificateGenerateProvider certGenerator = new CertificateGenerateProvider();
+            //certGenerator.GenerateRootCertificate(certificationConfiguration);
             Thread.Sleep(1);
+            //certies
+            Console.WriteLine("/loading / Certies");
+            int counter = 0;
+            int certIndex = 0;
+            CertificateExplorerProvider certificateExplorerProvider = new CertificateExplorerProvider(true);
+            certificateExplorerProvider.LoadCertificates();
+            foreach(var cert in certificateExplorerProvider.Certificates)
+            {
+                if(cert.PublishedFor == certificationConfiguration.CertName)
+                {
+                    certIndex = counter;
+                }
+                Console.WriteLine($"{counter++} >>> {cert}");
+
+
+            }
+            CertificateConfiguration certificationConfiguration2 = new CertificateConfiguration()
+            {
+                CertName = "RandomDepartment",
+                RootCertificate = certificateExplorerProvider.Certificates[certIndex].Certificate,
+                Password = "12345678",
+                CertLifeTime = 3,
+                OutFolder = @"C:\testcert"
+
+            };
+            CertificateGenerateProvider certGenerator2 = new CertificateGenerateProvider();
+            certGenerator2.GenerateCertificate(certificationConfiguration2);
+            Console.WriteLine("Done!");
         }
     }
 }
