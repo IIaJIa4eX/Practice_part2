55487b5290c2d0901691c7de2a2a8ba0af7e7832
Alexander <iiajia4ex@gmail.com>
2022-11-29 22:49:27 +0300

    added simple booking

--- Practice_part2/Restaurant/Program.cs
+++ Practice_part2/Restaurant/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Restaurant
+﻿using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Restaurant
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var rest = new RestaurantPlace();
+            while (true) {
+                Console.WriteLine("Как желаете забронировать столик? \n1- по смс(асинхоонно)\n2-в порядке очереди(синхронно) ");
+
+                if (!int.TryParse(Console.ReadLine(), out var choice) && choice is not (1 or 2))
+                {
+                    Console.WriteLine("Введите 1 или 2");
+                    continue;
+                }
+
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+
+                if (choice == 1)
+                {
+                    rest.BookFreeTableAsync(1);
+                }
+                else
+                {
+                    rest.BookFreeTable(1);
+                }
+
+                Console.WriteLine("Спасибо за Ваше обращение");
+                stopWatch.Stop();
+                TimeSpan elapsed = stopWatch.Elapsed;
+                Console.WriteLine($"{elapsed.Seconds:00}:{elapsed.Milliseconds:00}");
+            }
         }
     }
 }
---
+++ Practice_part2/Restaurant/RestaurantPlace.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class RestaurantPlace
+    {
+        private readonly List<Table> _tables = new();
+
+        public RestaurantPlace()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                _tables.Add(new Table(i));
+            }
+        }
+
+        public void BookFreeTable(int personsCount)
+        {
+            Console.WriteLine("Подождите, подмираем столик синхронно..");
+            var table = _tables.FirstOrDefault(t => t.SeatsCount > personsCount && t.state == State.Free);
+
+            Thread.Sleep(1000 * 5);
+
+            if(table is null)
+            {
+                Console.WriteLine("Простите, столов нет");
+            }
+            else
+            {
+                Console.WriteLine($"Готово! столик номер {table.Id}");
+            }
+        }
+
+        public void BookFreeTableAsync(int personsCount)
+        {
+            Console.WriteLine("Подождите, подмираем столик синхронно..");
+
+            Task.Run(async () =>
+            {
+                var table = _tables.FirstOrDefault(t => t.SeatsCount > personsCount && t.state == State.Free);
+
+
+                await Task.Delay(1000 * 5);
+                table?.SetState(State.Booked);
+
+                if (table is null)
+                {
+                    Console.WriteLine("смс - Простите, столов нет");
+                }
+                else
+                {
+                    Console.WriteLine($"смс - Готово! столик номер {table.Id}");
+                }
+            });
+        }
+    }
+}
