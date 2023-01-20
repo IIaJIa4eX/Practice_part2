fa0f1c793503fdcf82c719063414d0391bf613ec
Alexander <iiajia4ex@gmail.com>
2023-01-20 23:54:29 +0300

    added lesson 4

---
+++ Practice_part2/gRPC_Test_Clinic_net7/Program.cs
@@ -0,0 +1,91 @@
+using gRPC_Test_DataBase_Clinic;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using Microsoft.AspNetCore.Grpc.JsonTranscoding;
+using gRPC_Test_Clinic_net7.Services;
+using Microsoft.OpenApi.Models;
+
+namespace gRPC_Test_Clinic_net7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args); //First step: This step about webapp configuration(services, etc...)
+
+
+            //Http protocols configuration
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.Listen(IPAddress.Any, 5001, listenOptions =>
+                {
+                    listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+                });
+
+                options.Listen(IPAddress.Any, 5002, listenOptions =>
+                {
+                    listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1;
+                });
+
+            });
+
+            
+            builder.Services.AddGrpc();
+
+            //Transoding Http to Grpc format
+            builder.Services
+                .AddGrpc()
+                .AddJsonTranscoding();
+
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+
+
+            builder.Services.AddDbContext<ClinicDataBaseContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration["Settings:DataBaseOptions:ConnectionString"]);
+            });
+
+
+            builder.Services.AddGrpcSwagger();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo { Title = "gRPC Clinic", Version = "v1" });
+                //To make it works simply add <GenerateDocumentationFile>true</GenerateDocumentationFile> to project settings.
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "gRPC_Test_Clinic_net7.xml");//Name is same as Project's name
+                c.IncludeXmlComments(filePath);
+                c.IncludeGrpcXmlComments(filePath, includeControllerXmlComments: true);
+            });
+
+
+            var app = builder.Build();//Second step: This is my webapp after First step, this step about requests managing.
+
+
+            if (app.Environment.IsDevelopment())//gets from launchSettings.json (ASPNETCORE_ENVIRONMENT)
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
+            }
+
+            // Configure the HTTP request pipeline.
+
+            //allows to manage requests
+            app.UseRouting();
+
+            app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled= true });
+
+            app.MapGrpcService<ClinicService>().EnableGrpcWeb();
+            app.MapGet("/", () =>
+            "You need to create grps client to communicate with me"
+            );
+
+            app.Run();
+        }
+    }
+}
---
+++ Practice_part2/gRPC_Test_Clinic_net7/Services/ClinicService.cs
@@ -0,0 +1,122 @@
+﻿using Grpc.Core;
+using gRPC_Test_Clinic_net7.Protos;
+using gRPC_Test_DataBase_Clinic;
+using static gRPC_Test_Clinic_net7.Protos.ClinicService;
+
+namespace gRPC_Test_Clinic_net7.Services
+{
+    public class ClinicService : ClinicServiceBase
+    {
+        private readonly ClinicDataBaseContext _dbContext;
+
+
+
+        public ClinicService(ClinicDataBaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public override  Task<CreateClientResponse> CreateClient(CreateClientRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var client = new Client
+                {
+                    Document = request.Document,
+                    FirstName = request.Firstname,
+                    SurName = request.Surname
+                };
+
+                _dbContext.Clients.Add(client);
+                _dbContext.SaveChanges();
+
+                var response = new CreateClientResponse
+                {
+                    ClientId = client.Id,
+                    ErrorCode = 0,
+                    ErrorMessage = ""
+                };
+
+                return Task.FromResult(response);
+            }
+            catch (Exception ex)
+            {
+                var response = new CreateClientResponse
+                {
+                    ErrorCode = 1,
+                    ErrorMessage = $"{ex.Message}"
+                };
+
+                return Task.FromResult(response);
+            }
+        }
+
+        public override Task<GetClientsResponse> GetClients(GetClientsRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var response = new GetClientsResponse();
+
+                var clients = _dbContext.Clients.Select(x => new ClientResponse
+                {
+                    ClientId = x.Id,
+                    Document = x.Document,
+                    Firstname = x.FirstName,
+                    Surname = x.SurName
+                }).ToList();
+
+                response.Clients.AddRange(clients);
+
+                return Task.FromResult(response);
+            }
+            catch (Exception ex)
+            {
+                var response = new GetClientsResponse
+                {
+                    ErrorCode = 1,
+                    ErrorMessage = $"{ex.Message}"
+                };
+
+                return Task.FromResult(response);
+            }
+        }
+
+        public override Task<GetClientByIdResponse> GetByClientId(GetClientByIdRequest request, ServerCallContext context)
+        {
+            try
+            {
+                Client client = _dbContext.Clients
+                    .Where(cl => cl.Id == request.ClientId)
+                    .FirstOrDefault();
+
+                var client_response = new ClientResponse()
+                {
+                    ClientId = client.Id,
+                    Document = client.Document,
+                    Surname = client.SurName,
+                    Firstname = client.FirstName,
+                };
+
+                GetClientByIdResponse response = new GetClientByIdResponse()
+                {
+                    Client = client_response,
+                    ErrorCode= 0,
+                    ErrorMessage = ""
+                };
+
+                return Task.FromResult(response);
+            }
+            catch (Exception ex)
+            {
+                var response = new GetClientByIdResponse
+                {
+                    Client = null,
+                    ErrorCode = 1,
+                    ErrorMessage = $"{ex.Message}"
+                };
+
+                return Task.FromResult(response);
+            }
+        }
+    }
+}
